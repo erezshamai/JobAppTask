6b11b9f03cc64110308f39dcca5e2d274bdb1216
Erez Shamai <erezsh12@gmail.com>
2025-08-27 22:58:34 +0300

    remove redundent files

--- ci-cd/jenkins/shared-library/prepareEnv.groovy
+++
@@ -1,5 +0,0 @@
-def call(scmVars) {
-    env.GIT_COMMITTER = scmVars.GIT_COMMITTER
-    env.GIT_BRANCH = scmVars.GIT_BRANCH
-    echo "SCM triggered by: ${env.GIT_COMMITTER} on branch ${env.GIT_BRANCH}"
-}
