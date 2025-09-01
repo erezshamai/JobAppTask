e4c440fe3a741ca3cf456deea70ed812403e7e13
Erez Shamai <erezsh12@gmail.com>
2025-09-01 09:21:30 +0300

    Revert changes to app/main.py from commit ea235f7

--- app/main.py
+++ app/main.py
@@ -1,21 +1,11 @@
-import logging
 from flask import Flask, jsonify
 import docker
-
-# Configure logging
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
 
 app = Flask(__name__)
 
 @app.route('/running-containers', methods=['GET'])
 def get_running_containers():
     try:
-        # Log the request headers
-        logger.info("Request Headers:")
-        for header, value in request.headers.items():
-            logger.info(f"{header}: {value}")
-
         client = docker.from_env()  # Connects via /var/run/docker.sock
         running_containers = client.containers.list(filters={'status': 'running'})
         container_info = [
@@ -29,10 +19,6 @@ def get_running_containers():
         ]
         return jsonify({'running_containers': container_info})
     except docker.errors.DockerException as e:
-        # Log the request headers even in case of an error
-        logger.info("Request Headers (Error Case):")
-        for header, value in request.headers.items():
-            logger.info(f"{header}: {value}")        
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
