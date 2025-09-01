b3630db4fc3d34c1ad6178136fb3ab6f76f77fbb
Erez Shamai <erezsh12@gmail.com>
2025-09-01 15:13:07 +0300

    add X-Forwarded-For to response

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request  # Add request import
 import docker
 
 app = Flask(__name__)
@@ -17,9 +17,20 @@ def get_running_containers():
             }
             for container in running_containers
         ]
-        return jsonify({'running_containers': container_info})
+        # Create the response and add the X-Forwarded-For header
+        response = jsonify({'running_containers': container_info})
+        # Get X-Forwarded-For from the incoming request headers
+        x_forwarded_for = request.headers.get('X-Forwarded-For', '')
+        if x_forwarded_for:
+            response.headers['X-Forwarded-For'] = x_forwarded_for
+        return response
     except docker.errors.DockerException as e:
-        return jsonify({'error': str(e)}), 500
+        response = jsonify({'error': str(e)})
+        # Optionally add the header to error responses too
+        x_forwarded_for = request.headers.get('X-Forwarded-For', '')
+        if x_forwarded_for:
+            response.headers['X-Forwarded-For'] = x_forwarded_for
+        return response, 500
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=50001)
